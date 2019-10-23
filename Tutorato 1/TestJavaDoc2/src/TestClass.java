745c6fc990a281f73e568408d813b693463f96f5
Andrea Bombarda <a.bombarda@studenti.unibg.it>
2019-10-23 22:24:03 +0200

    Add Tutorato 3

--- Tutorato 1/TestJavaDoc2/src/TestClass.java
+++ Tutorato 1/TestJavaDoc2/src/TestClass.java
@@ -11,6 +11,22 @@ public class TestClass {
 	/** The second number. */
 	int b;
 	
+	public int getA() {
+		return a;
+	}
+
+	public void setA(int a) {
+		this.a = a;
+	}
+
+	public int getB() {
+		return b;
+	}
+
+	public void setB(int b) {
+		this.b = b;
+	}
+
 	/**
 	 * Instantiates a new test class.
 	 */
